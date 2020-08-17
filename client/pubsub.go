05522fa233a4e25790173620f20b84eea9339d29
Mark Chmarny <mark@chmarny.com>
2020-08-17 06:10:19 -0700

    multi pub/sub support #31

--- client/pubsub.go
+++ client/pubsub.go
@@ -7,8 +7,11 @@ import (
 	"github.com/pkg/errors"
 )
 
-// PublishEvent pubishes data onto specific pubsub topic.
-func (c *GRPCClient) PublishEvent(ctx context.Context, topic string, in []byte) error {
+// PublishEvent pubishes data onto topic in specific pubsub component.
+func (c *GRPCClient) PublishEvent(ctx context.Context, component, topic string, in []byte) error {
+	if component == "" {
+		return errors.New("nil component")
+	}
 	if topic == "" {
 		return errors.New("nil topic")
 	}
@@ -17,8 +20,9 @@ func (c *GRPCClient) PublishEvent(ctx context.Context, topic string, in []byte) 
 	}
 
 	envelop := &pb.PublishEventRequest{
-		Topic: topic,
-		Data:  in,
+		PubsubName: component,
+		Topic:      topic,
+		Data:       in,
 	}
 
 	_, err := c.protoClient.PublishEvent(authContext(ctx), envelop)
