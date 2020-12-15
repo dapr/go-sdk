b6b6d06110a4ec79bc80c8f412ebcb584624e3d7
landlord <rrylee1994@gmail.com>
2020-12-16 02:07:21 +0800

    feat: Invoke will extract querystring from method (#127)
    Co-authored-by: 李锐 <lirui@meican.com>

--- client/invoke_test.go
+++ client/invoke_test.go
@@ -35,14 +35,23 @@ func TestInvokeMethodWithContent(t *testing.T) {
 		assert.Nil(t, err)
 		assert.NotNil(t, resp)
 		assert.Equal(t, string(resp), data)
+	})
 
+	t.Run("with content, method contains querystring", func(t *testing.T) {
+		content := &DataContent{
+			ContentType: "text/plain",
+			Data:        []byte(data),
+		}
+		resp, err := testClient.InvokeMethodWithContent(ctx, "test", "fn?foo=bar&url=http://dapr.io", "get", content)
+		assert.Nil(t, err)
+		assert.NotNil(t, resp)
+		assert.Equal(t, string(resp), data)
 	})
 
 	t.Run("without content", func(t *testing.T) {
 		resp, err := testClient.InvokeMethod(ctx, "test", "fn", "get")
 		assert.Nil(t, err)
 		assert.Nil(t, resp)
-
 	})
 
 	t.Run("without service ID", func(t *testing.T) {
@@ -87,20 +96,78 @@ func TestInvokeMethodWithContent(t *testing.T) {
 
 func TestVerbParsing(t *testing.T) {
 	t.Run("valid lower case", func(t *testing.T) {
-		v := verbToHTTPExtension("post")
+		v := queryAndVerbToHTTPExtension("", "post")
 		assert.NotNil(t, v)
 		assert.Equal(t, v1.HTTPExtension_POST, v.Verb)
+		assert.Len(t, v.Querystring, 0)
 	})
 
 	t.Run("valid upper case", func(t *testing.T) {
-		v := verbToHTTPExtension("GET")
+		v := queryAndVerbToHTTPExtension("", "GET")
 		assert.NotNil(t, v)
 		assert.Equal(t, v1.HTTPExtension_GET, v.Verb)
 	})
 
 	t.Run("invalid verb", func(t *testing.T) {
-		v := verbToHTTPExtension("BAD")
+		v := queryAndVerbToHTTPExtension("", "BAD")
 		assert.NotNil(t, v)
 		assert.Equal(t, v1.HTTPExtension_NONE, v.Verb)
 	})
+
+	t.Run("valid query", func(t *testing.T) {
+		v := queryAndVerbToHTTPExtension("foo=bar&url=http://dapr.io", "post")
+		assert.NotNil(t, v)
+		assert.Equal(t, v1.HTTPExtension_POST, v.Verb)
+		assert.Len(t, v.Querystring, 2)
+		assert.Equal(t, "bar", v.Querystring["foo"])
+		assert.Equal(t, "http://dapr.io", v.Querystring["url"])
+	})
+}
+
+func TestExtractMethodAndQuery(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantMethod string
+		wantQuery  string
+	}{
+		{
+			"pure uri",
+			args{name: "method"},
+			"method",
+			"",
+		},
+		{
+			"root route method",
+			args{name: "/"},
+			"/",
+			"",
+		},
+		{
+			"uri with one query",
+			args{name: "method?foo=bar"},
+			"method",
+			"foo=bar",
+		},
+		{
+			"uri with two query",
+			args{name: "method?foo=bar&url=http://dapr.io"},
+			"method",
+			"foo=bar&url=http://dapr.io",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotMethod, gotQuery := extractMethodAndQuery(tt.args.name)
+			if gotMethod != tt.wantMethod {
+				t.Errorf("extractMethodAndQuery() gotMethod = %v, want %v", gotMethod, tt.wantMethod)
+			}
+			if gotQuery != tt.wantQuery {
+				t.Errorf("extractMethodAndQuery() gotQuery = %v, want %v", gotQuery, tt.wantQuery)
+			}
+		})
+	}
 }
