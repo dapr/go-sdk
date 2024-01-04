ea387fbe3248b1958b5c1afe279c746e63d60a7b
mikeee <hey@mike.ee>
2024-01-04 20:16:27 +0000

    fix: correct states
    Signed-off-by: mikeee <hey@mike.ee>

--- workflow/state_test.go
+++ workflow/state_test.go
@@ -12,55 +12,62 @@ func TestString(t *testing.T) {
 
 	t.Run("test running", func(t *testing.T) {
 		s := wfState.RuntimeStatus()
-		assert.Equal(t, "running", s.String())
+		assert.Equal(t, "RUNNING", s.String())
 	})
 
 	wfState.Metadata.RuntimeStatus = 1
 
 	t.Run("test completed", func(t *testing.T) {
 		s := wfState.RuntimeStatus()
-		assert.Equal(t, "completed", s.String())
+		assert.Equal(t, "COMPLETED", s.String())
 	})
 
 	wfState.Metadata.RuntimeStatus = 2
 
 	t.Run("test continued_as_new", func(t *testing.T) {
 		s := wfState.RuntimeStatus()
-		assert.Equal(t, "continued_as_new", s.String())
+		assert.Equal(t, "CONTINUED_AS_NEW", s.String())
 	})
 
 	wfState.Metadata.RuntimeStatus = 3
 
 	t.Run("test failed", func(t *testing.T) {
 		s := wfState.RuntimeStatus()
-		assert.Equal(t, "failed", s.String())
+		assert.Equal(t, "FAILED", s.String())
 	})
 
 	wfState.Metadata.RuntimeStatus = 4
 
 	t.Run("test canceled", func(t *testing.T) {
 		s := wfState.RuntimeStatus()
-		assert.Equal(t, "canceled", s.String())
+		assert.Equal(t, "CANCELED", s.String())
 	})
 
 	wfState.Metadata.RuntimeStatus = 5
 
 	t.Run("test terminated", func(t *testing.T) {
 		s := wfState.RuntimeStatus()
-		assert.Equal(t, "terminated", s.String())
+		assert.Equal(t, "TERMINATED", s.String())
 	})
 
 	wfState.Metadata.RuntimeStatus = 6
 
 	t.Run("test pending", func(t *testing.T) {
 		s := wfState.RuntimeStatus()
-		assert.Equal(t, "pending", s.String())
+		assert.Equal(t, "PENDING", s.String())
 	})
 
 	wfState.Metadata.RuntimeStatus = 7
 
 	t.Run("test suspended", func(t *testing.T) {
 		s := wfState.RuntimeStatus()
-		assert.Equal(t, "suspended", s.String())
+		assert.Equal(t, "SUSPENDED", s.String())
+	})
+
+	wfState.Metadata.RuntimeStatus = 8
+
+	t.Run("test unknown", func(t *testing.T) {
+		s := wfState.RuntimeStatus()
+		assert.Equal(t, "UNKNOWN", s.String())
 	})
 }
