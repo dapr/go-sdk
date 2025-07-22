469d47c466ea34e621b721885015ce861fac0541
Tiago Scolari <tiago@diagrid.io>
2025-07-22 10:13:33 +0100

    Add helpers to replace hand-made changes to dapr/dapr protos
    Signed-off-by: Tiago Scolari <tiago@diagrid.io>

---
+++ client/internal/crypto/helpers.go
@@ -0,0 +1,73 @@
+package crypto
+
+import (
+	commonv1pb "github.com/dapr/go-sdk/internal/proto/dapr/proto/common/v1"
+	runtimev1 "github.com/dapr/go-sdk/internal/proto/dapr/proto/runtime/v1"
+	"google.golang.org/protobuf/proto"
+)
+
+func GetPayload[T runtimev1.DecryptRequest | runtimev1.EncryptRequest | runtimev1.DecryptResponse | runtimev1.EncryptResponse](req *T) *commonv1pb.StreamPayload {
+	if req == nil {
+		return nil
+	}
+
+	switch r := any(req).(type) {
+	case *runtimev1.EncryptRequest:
+		return r.Payload
+	case *runtimev1.DecryptRequest:
+		return r.Payload
+	case *runtimev1.EncryptResponse:
+		return r.Payload
+	case *runtimev1.DecryptResponse:
+		return r.Payload
+	}
+
+	return nil
+}
+
+func SetPayload[T runtimev1.DecryptRequest | runtimev1.EncryptRequest | runtimev1.DecryptResponse | runtimev1.EncryptResponse](req *T, payload *commonv1pb.StreamPayload) {
+	if req == nil {
+		return
+	}
+
+	switch r := any(req).(type) {
+	case *runtimev1.EncryptRequest:
+		r.Payload = payload
+	case *runtimev1.DecryptRequest:
+		r.Payload = payload
+	case *runtimev1.EncryptResponse:
+		r.Payload = payload
+	case *runtimev1.DecryptResponse:
+		r.Payload = payload
+	}
+}
+
+func SetOptions[T runtimev1.DecryptRequest | runtimev1.EncryptRequest](req *T, opts proto.Message) {
+	if req == nil {
+		return
+	}
+
+	switch r := any(req).(type) {
+	case *runtimev1.EncryptRequest:
+		r.Options = opts.(*runtimev1.EncryptRequestOptions)
+	case *runtimev1.DecryptRequest:
+		r.Options = opts.(*runtimev1.DecryptRequestOptions)
+	}
+}
+
+func Reset[T runtimev1.DecryptRequest | runtimev1.EncryptRequest | runtimev1.DecryptResponse | runtimev1.EncryptResponse](msg *T) {
+	if msg == nil {
+		return
+	}
+
+	switch r := any(msg).(type) {
+	case *runtimev1.EncryptRequest:
+		r.Reset()
+	case *runtimev1.DecryptRequest:
+		r.Reset()
+	case *runtimev1.EncryptResponse:
+		r.Reset()
+	case *runtimev1.DecryptResponse:
+		r.Reset()
+	}
+}
---
+++ client/internal/crypto/helpers_test.go
@@ -0,0 +1,145 @@
+package crypto_test
+
+import (
+	"testing"
+
+	"github.com/dapr/go-sdk/client/internal/crypto"
+	commonv1 "github.com/dapr/go-sdk/internal/proto/dapr/proto/common/v1"
+	runtimev1 "github.com/dapr/go-sdk/internal/proto/dapr/proto/runtime/v1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+)
+
+func TestPayloadMethods(t *testing.T) {
+	testCases := map[string]struct {
+		protoMsg  any
+		inputData []byte
+	}{
+		"EncryptRequest": {
+			protoMsg:  &runtimev1.EncryptRequest{},
+			inputData: []byte("test data"),
+		},
+		"EncryptResponse": {
+			protoMsg:  &runtimev1.EncryptResponse{},
+			inputData: []byte("test data"),
+		},
+		"DecryptRequest": {
+			protoMsg:  &runtimev1.DecryptRequest{},
+			inputData: []byte("test data"),
+		},
+		"DecryptResponse": {
+			protoMsg:  &runtimev1.DecryptResponse{},
+			inputData: []byte("test data"),
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			inputPayload := &commonv1.StreamPayload{Data: tc.inputData}
+			var outputPayload *commonv1.StreamPayload
+
+			switch r := tc.protoMsg.(type) {
+			case *runtimev1.EncryptRequest:
+				crypto.SetPayload(r, inputPayload)
+				outputPayload = crypto.GetPayload(r)
+			case *runtimev1.EncryptResponse:
+				crypto.SetPayload(r, inputPayload)
+				outputPayload = crypto.GetPayload(r)
+			case *runtimev1.DecryptRequest:
+				crypto.SetPayload(r, inputPayload)
+				outputPayload = crypto.GetPayload(r)
+			case *runtimev1.DecryptResponse:
+				crypto.SetPayload(r, inputPayload)
+				outputPayload = crypto.GetPayload(r)
+			default:
+				require.Failf(t, "unsupported proto message type", "the type was %T", r)
+			}
+
+			assert.Equal(t, tc.inputData, outputPayload.Data, "payload should match the input")
+		})
+	}
+}
+
+func TestSetOptions(t *testing.T) {
+	testCases := map[string]struct {
+		protoMsg any
+		options  proto.Message
+	}{
+		"EncryptRequest": {
+			protoMsg: &runtimev1.EncryptRequest{},
+			options: &runtimev1.EncryptRequestOptions{
+				KeyName: "testing",
+			},
+		},
+		"DecryptRequest": {
+			protoMsg: &runtimev1.DecryptRequest{},
+			options: &runtimev1.DecryptRequestOptions{
+				KeyName: "testing",
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			var outputOptions proto.Message
+
+			switch r := tc.protoMsg.(type) {
+			case *runtimev1.EncryptRequest:
+				crypto.SetOptions(r, tc.options)
+				outputOptions = r.Options
+			case *runtimev1.DecryptRequest:
+				crypto.SetOptions(r, tc.options)
+				outputOptions = r.Options
+			default:
+				require.Failf(t, "unsupported proto message type", "the type was %T", r)
+			}
+
+			assert.Equal(t, tc.options, outputOptions, "options should be persisted")
+		})
+	}
+}
+
+func TestReset(t *testing.T) {
+	testCases := map[string]struct {
+		protoMsg any
+	}{
+		"EncryptRequest": {
+			protoMsg: &runtimev1.EncryptRequest{Payload: &commonv1.StreamPayload{Data: []byte("test data")}},
+		},
+		"EncryptResponse": {
+			protoMsg: &runtimev1.EncryptResponse{Payload: &commonv1.StreamPayload{Data: []byte("test data")}},
+		},
+		"DecryptRequest": {
+			protoMsg: &runtimev1.DecryptRequest{Payload: &commonv1.StreamPayload{Data: []byte("test data")}},
+		},
+		"DecryptResponse": {
+			protoMsg: &runtimev1.DecryptResponse{Payload: &commonv1.StreamPayload{Data: []byte("test data")}},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			var payload *commonv1.StreamPayload
+
+			switch r := tc.protoMsg.(type) {
+			case *runtimev1.EncryptRequest:
+				crypto.Reset(r)
+				payload = crypto.GetPayload(r)
+			case *runtimev1.EncryptResponse:
+				crypto.Reset(r)
+				payload = crypto.GetPayload(r)
+			case *runtimev1.DecryptRequest:
+				crypto.Reset(r)
+				payload = crypto.GetPayload(r)
+			case *runtimev1.DecryptResponse:
+				crypto.Reset(r)
+				payload = crypto.GetPayload(r)
+			default:
+				require.Failf(t, "unsupported proto message type", "the type was %T", r)
+			}
+
+			assert.Nil(t, payload)
+		})
+	}
+}
