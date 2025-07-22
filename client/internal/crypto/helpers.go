9440b1fae7747f680a834d8067a22e2b0f1d479a
Tiago Scolari <tiago@diagrid.io>
2025-07-22 11:43:09 +0100

    Add comments to the new crypto package
    Signed-off-by: Tiago Scolari <tiago@diagrid.io>

--- client/internal/crypto/helpers.go
+++ client/internal/crypto/helpers.go
@@ -1,3 +1,8 @@
+// Package crypto was introduced when removing the code dependency to the
+// generated protos from the dapr/dapr repository.
+// It contains helper methods that are meant to replace the custom changes
+// introduced by the protos on that repo at:
+// https://github.com/dapr/dapr/blob/db5361701c55cae8ad21be60e2d4557f98cbc741/pkg/proto/runtime/v1/dapr_additional.go.
 package crypto
 
 import (
@@ -7,6 +12,7 @@ import (
 	runtimev1 "github.com/dapr/go-sdk/internal/proto/dapr/proto/runtime/v1"
 )
 
+// GetPayload will retrieve the payload from one of the given runtime proto message.
 func GetPayload[T runtimev1.DecryptRequest | runtimev1.EncryptRequest | runtimev1.DecryptResponse | runtimev1.EncryptResponse](req *T) *commonv1pb.StreamPayload {
 	if req == nil {
 		return nil
@@ -26,6 +32,7 @@ func GetPayload[T runtimev1.DecryptRequest | runtimev1.EncryptRequest | runtimev
 	return nil
 }
 
+// SetPayload will set the payload for one of the given runtime proto message.
 func SetPayload[T runtimev1.DecryptRequest | runtimev1.EncryptRequest | runtimev1.DecryptResponse | runtimev1.EncryptResponse](req *T, payload *commonv1pb.StreamPayload) {
 	if req == nil {
 		return
@@ -43,6 +50,7 @@ func SetPayload[T runtimev1.DecryptRequest | runtimev1.EncryptRequest | runtimev
 	}
 }
 
+// SetOptions will set the options for one of the given runtime proto message.
 func SetOptions[T runtimev1.DecryptRequest | runtimev1.EncryptRequest](req *T, opts proto.Message) {
 	if req == nil {
 		return
@@ -56,6 +64,7 @@ func SetOptions[T runtimev1.DecryptRequest | runtimev1.EncryptRequest](req *T, o
 	}
 }
 
+// Reset will call the `Reset` method in the given runtime proto message.
 func Reset[T runtimev1.DecryptRequest | runtimev1.EncryptRequest | runtimev1.DecryptResponse | runtimev1.EncryptResponse](msg *T) {
 	if msg == nil {
 		return
